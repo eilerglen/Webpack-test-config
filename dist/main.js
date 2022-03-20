3e972f67c0c33590b503a603c4cc75ab561be449
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-03-20 20:44:53 +0300

    Babel is done!

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("Hey!");
+(()=>{console.log("Hey!");var o=[2,3,5].map((function(o){return 2*o}));console.log(o)})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log("Hey!")
+
+const numbers = [2,3,5]
+
+const doubleNumbers = numbers.map(item => item*2);
+
+console.log(doubleNumbers)
